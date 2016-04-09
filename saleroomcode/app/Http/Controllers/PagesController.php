8d77c755c89fd5ae2861eb090d659281e389b532
Paul Steele <u1273843@unimail.hud.ac.uk>
2016-04-09 20:39:39 +0100

    first controller pages laravel style

--- saleroomcode/app/Http/Controllers/PagesController.php
+++ saleroomcode/app/Http/Controllers/PagesController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
-use App\Http\Requests;
+//use App\Http\Requests;
 
 class PagesController extends Controller
 {
-    //
+    public function home()
+    {
+    	//$people = ['paul','fred','david',]
+			return view('welcome');
+    	//return view('welcome', compact('people'));
+    }
+
+    public function about()
+    {
+    	//$people = ['one','two','three',]
+    		return view('Pages.about');
+    	//return view('pages.about', compact('people'));
+    }
 }
